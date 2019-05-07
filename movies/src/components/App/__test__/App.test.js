0469cd11659d71099a42b2563c679f60603f180d
xavierDelaFuente <asnewyla@gmail.com>
2019-05-07 21:47:30 +0200

    feature:add home and a router

--- movies/src/components/App/__test__/App.test.js
+++ movies/src/components/App/__test__/App.test.js
@@ -1,9 +1,33 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { mount } from "enzyme";
 import App from "../";
+import { MemoryRouter } from "react-router-dom";
+import { welcomeMessage } from "../../../texts/eng.json";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("[Component]: App:", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(
+      <MemoryRouter initialEntries={["/"]}>
+        <App />
+      </MemoryRouter>
+    );
+  });
+
+  test("renders without crashing", () => {
+    expect(wrapper).toHaveLength(1);
+  });
+
+  describe("Rerders desired components:", () => {
+    test("renders a Link to the Board", () => {
+      expect(wrapper.find("Link").props().to).toBe("/");
+    });
+
+    test("renders a welcome message", () => {
+      expect(
+        wrapper.find(`[data-testid='welcome-message']`).props().children
+      ).toEqual(welcomeMessage);
+    });
+  });
 });
